756d5b92fa052fa91807cac64e7a5dbacd149f68
alfianrma77 <alfianrma77@gmail.com>
2023-11-27 10:21:33 +0700

    first commit

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
+import CheckAuth from './CheckAuth';
+import UnderConstruction from '../components/UnderConstruction';
+import AdminLayout from '../layouts/AdminLayouts';
+import SignIn from '../pages/signin/SignIn';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<CheckAuth />} />
+      <Route path="*" element={<UnderConstruction />} />
+      <Route
+        path="signin"
+        element={
+          <CheckAuth>
+            <SignIn />
+          </CheckAuth>
+        }
+      />
+      <Route path="admin/*" element={<AdminLayout />}>
+        <Route path="*" element={<UnderConstruction />} />
+        <Route path="course" element={<UnderConstruction />} />
+        <Route path="dashboard" element={<UnderConstruction />} />
+        <Route path="students" element={<UnderConstruction />} />
+        <Route path="payment" element={<UnderConstruction />} />
+        <Route path="report" element={<UnderConstruction />} />
+        <Route path="settings" element={<UnderConstruction />} />
+      </Route>
+    </Routes>
+  );
+};
